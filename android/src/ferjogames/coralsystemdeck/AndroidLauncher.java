e5e8cf4dcfcd87c16accd00876ea648906f7a7ad
Fernando Garcia Alvarez <fercarcedo@gmail.com>
2018-10-06 00:45:45 +0200

    Remove Crashlytics

--- android/src/ferjogames/coralsystemdeck/AndroidLauncher.java
+++ android/src/ferjogames/coralsystemdeck/AndroidLauncher.java
@@ -2,18 +2,14 @@ package ferjogames.coralsystemdeck;
 
 import android.os.Bundle;
 import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Fabric.with(this, new Crashlytics());
 		FrameLayout layout = new FrameLayout(this);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		layout.addView(initializeForView(new CoralSystemDeck(new Android(this)), config));
